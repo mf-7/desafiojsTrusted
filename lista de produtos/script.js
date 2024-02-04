9bf723df2a03b514dc4792e077ac5932ef5b43d6
MF <44318981+mf-7@users.noreply.github.com>
2024-02-04 20:46:25 +0000

    Add files via upload

--- lista de produtos/script.js
+++ lista de produtos/script.js
@@ -1,28 +1,19 @@
 const produtos = [
-  { nome: "Computador", categoria: "Tecnologia", preco: "800" },
-  { nome: "Telemóvel", categoria: "Tecnologia", preco: "8" },
-  { nome: "TV", categoria: "Tecnologia", preco: "8000,1" },
+  { nome: "Curso Outsystems", categoria: "Programação", preco: "4000€" },
+  { nome: "Telemóvel", categoria: "Tecnologia", preco: "40000€" },
+  { nome: "TV", categoria: "Tecnologia", preco: "8000,1€" },
+  { nome: "Cadeira", categoria: "Mobiliário", preco: "7001€" },
+  { nome: "Teclado", categoria: "Tecnologia", preco: "1000001€" },
 ];
 
-let produtoTabela = document.getElementById("produtoLista");
+let produtosTabela = document.getElementById("produtoLista");
 
 for (i = 0; i < produtos.length; i++) {
-  let produtoTableLinha = document.createElement("tr");
-  let produtoTabelaDadosNome = document.createElement("td");
-  produtoTabelaDadosNome.innerHTML = produtos[i].nome;
-  produtoTableLinha.appendChild(produtoTabelaDadosNome);
-
-  produtoTabela.appendChild(produtoTableLinha);
-
-  let produtoTabelaDadosCategoria = document.createElement("td");
-  produtoTabelaDadosCategoria.innerHTML = produtos[i].categoria;
-  produtoTableLinha.appendChild(produtoTabelaDadosCategoria);
-
-  produtoTabela.appendChild(produtoTableLinha);
-
-  let produtoTabelaDadosPreco = document.createElement("td");
-  produtoTabelaDadosPreco.innerHTML = produtos[i].preco;
-  produtoTableLinha.appendChild(produtoTabelaDadosPreco);
-
-  produtoTabela.appendChild(produtoTableLinha);
+  let linha = produtosTabela.insertRow();
+  let nome = linha.insertCell();
+  let categoria = linha.insertCell();
+  let preco = linha.insertCell();
+  nome.innerHTML = produtos[i].nome;
+  categoria.innerHTML = produtos[i].categoria;
+  preco.innerHTML = produtos[i].preco;
 }
